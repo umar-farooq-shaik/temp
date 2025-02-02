56732e59f629271cc7de4e26a12bc1d208e60bcf
umar-farooq-shaik <umarfarooqshaik665@gmail.com>
2025-02-02 13:49:30 +0530

    adding files

---
+++ bolt_new_clone/app/provider.jsx
@@ -0,0 +1,53 @@
+"use client"
+import React, {useEffect, useState} from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import Header from "@/components/custom/Header";
+import { MessagesContext } from '../context/MessagesContext';
+import { UserDetailContext } from '../context/UserDetailContext';
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { useConvex } from "convex/react";
+import { api } from "@/convex/_generated/api";
+function Provider({children}){
+    const [messages,setMessages]=useState();
+    const [userDetail,setUserDetail]=useState();
+    const convex=useConvex();
+
+    useEffect(()=>{
+        IsAuthenticated();
+    },[])
+
+    const IsAuthenticated=async()=>{
+        if(typeof window!==undefined)
+            {
+            const user=JSON.parse(localStorage.getItem('user'))
+            //fetch from database
+            const result=await convex.query(api.users.GetUser,{
+                email:user?.email
+            })
+            setUserDetail(result);
+            console.log(result);
+        }
+    }
+
+    return(
+        <div>
+            <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID_KEY}>
+            <UserDetailContext.Provider value={{userDetail,setUserDetail}}>
+            <MessagesContext.Provider value={{messages,setMessages}}>
+            <NextThemesProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+            >
+            <Header/>
+            {children}
+            </NextThemesProvider>
+                </MessagesContext.Provider>
+                </UserDetailContext.Provider>
+                </GoogleOAuthProvider>
+        </div>
+    )
+}
+
+export default Provider
---
+++ bolt_new_clone/components/custom/SignInDialog.jsx
@@ -0,0 +1,73 @@
+import React, { useContext } from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import Lookup from '@/data/Lookup'
+import { Button } from '../ui/button'
+import { useGoogleLogin } from '@react-oauth/google';
+import { UserDetailContext } from '../../context/UserDetailContext';
+import axios from 'axios';
+import { CreateUser } from '../../convex/users';
+import { useMutation } from 'convex/react';
+import uuid4 from 'uuid4';
+import { api } from '@/convex/_generated/api';
+
+function SignInDialog({openDialog,closeDialog}) {
+const {UserDetail,setUserDetail}=useContext(UserDetailContext)   ; 
+const CreateUser = useMutation(api.users.CreateUser);    
+const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log(tokenResponse);
+      const userInfo = await axios.get(
+        'https://www.googleapis.com/oauth2/v3/userinfo',
+        { headers: { Authorization: 'Bearer'+tokenResponse?.access_token } },
+      );
+  
+      console.log(userInfo);
+      const user=userInfo.data;
+      await CreateUser({
+        name:user?.name,
+        email:user?.email,
+        picture:user?.picture,
+        uid:uuid4()
+      }) 
+
+      if(typeof window!==undefined)
+      {
+        localStorage.setItem('user',JSON.stringify(user))
+      }
+      setUserDetail(userInfo?.data);
+      //save this inside out database
+      closeDialog(false);
+    },
+    onError: errorResponse => console.log(errorResponse),
+  });
+    return (
+        <Dialog open={openDialog} onOpenChange={closeDialog}>
+               
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle></DialogTitle>
+                    <div>
+                        <div className="flex flex-col items-center justify-center gap-3">
+                        <h2 className='font-bold text-2xl text-center text-white'>{Lookup.SIGNIN_HEADING}</h2>
+                        <p className='mt-2 text-center'>{Lookup.SIGNIN_SUBHEADING}</p>
+                        <Button className="bg-blue-500 text-white hover:bg-blue-400
+                        mt-3 " onClick={googleLogin}>Sign In With Google</Button>
+
+                        <p>{Lookup?.SIGNIN_AGREEMENT_TEXT}</p>
+                        </div>
+                    </div>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+
+    )
+}
+
+export default SignInDialog
