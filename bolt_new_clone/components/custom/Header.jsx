0048b39718536a60f4c340dd3af4493e818d83b5
umar-farooq-shaik <umarfarooqshaik645@gmail.com>
2025-02-03 21:13:40 +0530

    till the response message on left

--- bolt_new_clone/components/custom/Header.jsx
+++ bolt_new_clone/components/custom/Header.jsx
@@ -1,22 +1,31 @@
-import Image from 'next/image'
-import React ,{ useContext } from 'react';
-import { Button } from '../ui/button'
-import Colors from '@/data/Colors'
-import { UserDetailContext } from '@/context/UserDetailContext'
+import Image from 'next/image';
+import React, { useContext } from 'react';
+import { Button } from '../ui/button';
+import Colors from '@/data/Colors';
+import { UserDetailContext } from '@/context/UserDetailContext';
 
 function Header() {
-  const {userDetail,setUserDetail}=useContext(UserDetailContext);
+  const { userDetail = {}, setUserDetail } = useContext(UserDetailContext); // Provide a default value
+
   return (
     <div className='p-4 flex justify-between items-center'>
-      <Image src={'/logo.png'} alt='Logo' width={40} height={40}/>
-      {!userDetail?.name &&<div className='flex gap-5'>
-        <Button variant="ghost">Sign In</Button>
-        <Button className="text-white" style={{
-            backgroundColor:Colors.BLUE
-        }}>Get Started</Button>
-      </div>}
+      <Image src={'/logo.png'} alt='Logo' width={40} height={40} />
+      {!userDetail.name && (
+        <div className='flex gap-5'>
+          <Button variant="ghost">Sign In</Button>
+          <Button
+            className="text-white"
+            style={{ backgroundColor: Colors.BLUE }}
+          >
+            Get Started
+          </Button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
+
+
+
